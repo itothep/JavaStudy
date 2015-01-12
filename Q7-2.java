6004f3e0c85c570f534d7aa71550875fc8bd16fd
unknown <Jungdeokho@gmail.com>
2015-01-12 21:28:33 +0900

    add bankrupt func ( 파산 기능 추가 )
    플레이어의 구슬 개수가 게임결과 음수가 되면 파산처리가 되도록 하였습니다.
    If number of biz come to negative num, It will consider its statue as bankrupt

--- Q7-2.java
+++ Q7-2.java
@@ -1,30 +1,39 @@
 class Child {
 	int bizNum;
 	public Child(int num) {
-		bizNum=num;
+		bizNum = num;
 	}
 	public void ShowResult(Child player, int n) {
-	System.out.println("플레이어 "+n);
-	System.out.println("구슬 개수: "+player.bizNum);
+		System.out.println("플레이어 " + n);
+		if (player.bizNum == 0) {
+			System.out.println("파산");
+		} else System.out.println("구슬 개수: " + player.bizNum);
 	}
 }
 
 class Main {
+	public static int outPoint = 0;
 	public static void GameResult(Child winner, Child loser, int num) {
-		winner.bizNum+=num;
-		loser.bizNum-=num;
+		winner.bizNum += num;
+		loser.bizNum -= num;
+		if (loser.bizNum <= 0) {
+			loser.bizNum = 0;
+			outPoint = 1;
 		}
-	
+	}
+
 	public static void main(String[] args) {
-	Child player1=new Child(15);
-	Child player2=new Child(9);
-	// Round 1
-	GameResult(player1, player2, 2);
-	// Round 2
-	GameResult(player2, player1, 7);
-	// Result
-	player1.ShowResult(player1, 1);
-	player2.ShowResult(player2, 2);
+		Child player1 = new Child(15);
+		Child player2 = new Child(9);
+		// Round 1
+		GameResult(player1, player2, 2);
+		if (outPoint != 1) {
+			// Round 2
+			GameResult(player2, player1, 17);
+		}
+		// Result
+		player1.ShowResult(player1, 1);
+		player2.ShowResult(player2, 2);
 	}
 
 }
